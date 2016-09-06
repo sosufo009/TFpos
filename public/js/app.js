275a8bb5cb6c12dd105bf23835d95ae5f3657907
sosufo009 <sosufo009@yahoo.com.tw>
2016-09-07 00:01:41 +0200

    20160906

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,32 @@
-angular.module('app',[])
-.controller('test',[function(){
-  var self = this;
-  // alert('herllo');
-  var data = {
-      "email": "john@stamplay.com",
-      "password": "john123"
-  }
+angular.module('app', ['ngAnimate'])
+    .controller('test', [function() {
+        var self = this;
+        // alert('herllo');
+        // var data = {
+        //     "email": "john@stamplay.com",
+        //     "password": "john123"
+        // }
+        //
+        // self.login = function() {
+        //     Stamplay.User.signup(data, function(error, result) {
+        //         //manage the result and the error
+        //         alert(result);
+        //     })
+        // }
 
-  self.login = function()
-  {
-    Stamplay.User.signup(data, function(error, result) {
-        //manage the result and the error
-        alert(result);
-    })
-  }
+    }])
+    .controller('order', [function() {
+        var self = this;
 
-}]);
+
+        self.orderListOpenStatus = true;
+
+        self.openArrow = function() {
+
+            if (self.orderListOpenStatus == true) {
+                self.orderListOpenStatus = false;
+            } else if (self.orderListOpenStatus != true) {
+                self.orderListOpenStatus = true;
+            }
+        }
+    }]);
