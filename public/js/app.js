3e3aaac2bcef12c1225c7c10ddca2e03ddfcfca3
sosufo009 <sosufo009@yahoo.com.tw>
2016-09-06 19:14:47 +0200

    20160906

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,17 @@ angular.module('app',[])
 .controller('test',[function(){
   var self = this;
   // alert('herllo');
+  var data = {
+      "email": "john@stamplay.com",
+      "password": "john123"
+  }
+
+  self.login = function()
+  {
+    Stamplay.User.signup(data, function(error, result) {
+        //manage the result and the error
+        alert(result);
+    })
+  }
+
 }]);
